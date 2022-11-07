586f811ff2c0f79e4d987eb02e9eb02bb438c5dc
Ivan <114409228+IvanBondarenkoIT@users.noreply.github.com>
2022-11-07 19:41:30 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pandas
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet = {row[0]: row[1] for (index, row) in df.iterrows()}
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Enter word - ").upper()
-print([alphabet.get(i) for i in user_word])
 
+
+def generate_phonetic():
+    user_word = input("Enter word - ").upper()
+    try:
+        print([alphabet.get(i) for i in user_word])
+    except IndexError: 
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    
+
+generate_phonetic()
